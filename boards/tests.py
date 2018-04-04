7f809b88defa0729f967a6a481a70e087849233a
Jerry Tsen <ijerrycen@gmail.com>
2018-04-04 16:50:11 +0800

    add tests and homepage

--- boards/tests.py
+++ boards/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from django.core.urlresolvers import reverse
+from django.urls import resolve
 
-# Create your tests here.
+from boards.views import home
+
+
+class HomeTests(TestCase):
+    def test_home_view_status_code(self):
+        url = reverse('home')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_url_resolves_home_view(self):
+        view = resolve('/')
+        self.assertEquals(view.func, home)
--- boards/views.py
+++ boards/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+from boards.models import Board
+
+
+def home(request):
+    boards = Board.objects.all()
+    return render(request, 'home.html', {'boards': boards})
+
+
